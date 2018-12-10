f841a0057b8bc4e3114144a4c35ae427e82562c7
jcbespinosa1999 <45276309+jcbespinosa1999@users.noreply.github.com>
2018-12-10 09:28:10 +0800

    Add files via upload

---
+++ AddJewelry.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace OOP_PROJECT
+{
+    /// <summary>
+    /// Interaction logic for AddJewelry.xaml
+    /// </summary>
+    public partial class AddJewelry : UserControl
+    {
+        public AddJewelry()
+        {
+            InitializeComponent();
+        }
+    }
+}
